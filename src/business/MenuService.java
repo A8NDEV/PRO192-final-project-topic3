5b4845ec09432c9acde782182d95fb3f4e79e657
ANDEV09 <lamhoangan612@gmail.com>
2026-09-27 01:38:57 +0700

    feat: add addItem with duplicate id check

--- src/business/MenuService.java
+++ src/business/MenuService.java
@@ -4,10 +4,38 @@
  */
 package business;
 
+import java.util.ArrayList;
+import java.util.List;
+import models.MenuItem;
+import utils.CustomException;
+
 /**
  *
  * @author ASUS
  */
+/**
+ * Xử lý nghiệp vụ quản lý menu: lưu danh sách món. và thực hiện các thao tác
+ * CRUD.
+ */
 public class MenuService {
-    
+
+    private List<MenuItem> menuList;
+
+    public MenuService(List<MenuItem> menuList) {
+        this.menuList = menuList;
+    }
+
+    public void addItem(MenuItem item) throws CustomException {
+        for (MenuItem m : menuList) {
+            if (m.getItemId().equalsIgnoreCase(item.getItemId())) {
+                throw new CustomException("Item ID '" + item.getItemId() + "' already exists!");
+            }
+        }
+        menuList.add(item);
+    }
+
+    public List<MenuItem> getMenuList() {
+        return new ArrayList<>(menuList);
+    }
+
 }
