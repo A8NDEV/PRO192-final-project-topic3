560ba12b196ca621997487530f5fa2d65d5f626b
ANDEV09 <lamhoangan612@gmail.com>
2026-09-27 01:36:45 +0700

    feat: add MenuItem model

--- src/models/MenuItem.java
+++ src/models/MenuItem.java
@@ -9,5 +9,64 @@ package models;
  * @author ASUS
  */
 public class MenuItem {
+    private String itemId;
+    private String itemName;
+    private String category;
+    private double price;
+    private boolean status;
+
+    public MenuItem(String itemId, String itemName, String category, double price, boolean status) {
+        this.itemId = itemId;
+        this.itemName = itemName;
+        this.category = category;
+        this.price = price;
+        this.status = status;
+    }
+
+    public String getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(String itemId) {
+        this.itemId = itemId;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
     
+    @Override
+    public String toString(){
+         return "ID: " + itemId + ", Name: " + itemName + ", Category: " + category
+            + ", Price: " + price + ", Status: " + (status ? "Available" : "Unavailable");
+    }
 }
+
